9b706fd9055ded4b8419c2a867e6b60831b2aa1a
CJDev <colin.goehler@gmail.com>
2026-03-19 19:09:21 +0100

    Added instancing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use jni::elements::ReleaseMode;
-use jni::objects::{JByteArray, JClass, JObject};
-use jni::sys::{jboolean, jlong, JNI_TRUE};
-use jni::EnvUnowned;
-use wasmer::{imports, Instance, Module, Store, Value};
+use jni::objects::{JByteArray, JClass, JLongArray, JObject, JString};
+use jni::strings::JNIString;
+use jni::sys::jlong;
+use jni::{jni_str, EnvUnowned};
+use wasmer::{imports, Instance, Module, Store};
 
 #[allow(non_snake_case)]
 #[unsafe(no_mangle)]
@@ -15,7 +16,7 @@ pub extern "system" fn Java_de_cjdev_wasm_WasmStore_default_1store<'caller>(
 
 #[allow(non_snake_case)]
 #[unsafe(no_mangle)]
-pub extern "system" fn Java_de_cjdev_wasm_WasmStore_free_1store<'caller>(
+pub extern "system" fn Java_de_cjdev_wasm_WasmStore_free<'caller>(
     _unowned_env: EnvUnowned<'caller>,
     _this: JObject<'caller>,
     ptr: jlong
@@ -30,14 +31,13 @@ pub extern "system" fn Java_de_cjdev_wasm_WasmStore_free_1store<'caller>(
 
 #[allow(non_snake_case)]
 #[unsafe(no_mangle)]
-pub extern "system" fn Java_de_cjdev_wasm_WasmNative_run_1wasm<'caller>(
+pub extern "system" fn Java_de_cjdev_wasm_WasmStore_load<'caller>(
     mut unowned_env: EnvUnowned<'caller>,
-    _class: JClass<'caller>,
+    _this: JObject<'caller>,
     store: jlong,
     wasm_bytes: JByteArray<'caller>
-) -> jboolean {
-
-    let outcome = unowned_env.with_env(|env| -> Result<jboolean, jni::errors::Error> {
+) -> jlong {
+    let outcome = unowned_env.with_env(|env| -> Result<jlong, jni::errors::Error> {
         let store = unsafe { &mut *(store as *mut Store) };
         let elements = unsafe {
             &wasm_bytes.get_elements(env, ReleaseMode::NoCopyBack)?
@@ -51,12 +51,78 @@ pub extern "system" fn Java_de_cjdev_wasm_WasmNative_run_1wasm<'caller>(
         let import_object = imports! {};
         let instance = Instance::new(store, &module, &import_object).unwrap();
 
-        let sum_fn = instance.exports.get_function("main").unwrap();
-        sum_fn.call(store, &[]).unwrap();
-        return Ok(JNI_TRUE);
+        return Ok(Box::into_raw(Box::new(instance)) as jlong);
     });
 
     outcome.resolve::<jni::errors::ThrowRuntimeExAndDefault>()
+}
+
+#[allow(non_snake_case)]
+#[unsafe(no_mangle)]
+pub extern "system" fn Java_de_cjdev_wasm_WasmInstance_run<'caller>(
+    mut _unowned_env: EnvUnowned<'caller>,
+    _class: JClass<'caller>,
+    store: jlong,
+    instance: jlong,
+    function_name: JString<'caller>
+) -> JLongArray<'caller> {
+    let store = unsafe { &mut *(store as *mut Store) };
+    let instance = unsafe { &mut *(instance as *mut Instance) };
+    let function_name = function_name.to_string();
+    let function = instance.exports.get_function(function_name.as_str());
+    _unowned_env.with_env(|env| -> Result<JLongArray, jni::errors::Error> {
+        let function = match function {
+            Ok(f) => f,
+            Err(_) => {
+                env.throw_new(
+                    jni_str!("java/lang/RuntimeException"),
+                    JNIString::from(format!(
+                        "Function '{}' not found",
+                        function_name
+                    ))
+                )?;
+                return Ok(JLongArray::null());
+            },
+        };
+
+        let results = match function.call(store, &[]) {
+            Ok(r) => r,
+            Err(e) => {
+                env.throw_new(
+                    jni_str!("java/lang/RuntimeException"),
+                    JNIString::from(format!(
+                        "Wasm call '{}'",
+                        e
+                    ))
+                )?;
+                return Ok(JLongArray::null());
+            }
+        };
+
+
+        let values: Vec<i64> = results
+            .iter()
+            .map(|v| v.unwrap_i64())
+            .collect();
+        let array = env.new_long_array(values.len())?;
+        array.set_region(env, 0, &values)?;
+        return Ok(array);
+    }).resolve::<jni::errors::ThrowRuntimeExAndDefault>()
+}
+
+#[allow(non_snake_case)]
+#[unsafe(no_mangle)]
+pub extern "system" fn Java_de_cjdev_wasm_WasmInstance_free<'caller>(
+    _unowned_env: EnvUnowned<'caller>,
+    _this: JObject<'caller>,
+    ptr: jlong
+) {
+    let ptr = ptr as *mut Instance;
+    if !ptr.is_null() {
+        unsafe {
+            drop(Box::from_raw(ptr));
+        }
+    }
 }
 
 #[cfg(test)]
